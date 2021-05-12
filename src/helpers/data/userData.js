75f126d6fd1d37816ad5d1ac2207bb9400480160
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-05-11 19:27:59 -0500

    create promise for dmUsers

--- src/helpers/data/userData.js
+++ src/helpers/data/userData.js
@@ -3,9 +3,15 @@ import firebaseConfig from '../apiKeys';
 
 const dbUrl = firebaseConfig.databaseURL;
 
-const getDirectMessageUsers = () => {
+const getDirectMessageUsers = () => new Promise((resolve, reject) => {
   axios(`${dbUrl}/directMessageUsers.json`)
     .then((response) => {
+      if (response.data) {
+        resolve(Object.values(response.data));
+      } else {
+        resolve([]);
+      }
+    }).catch((error) => reject(error));
+});
 
-    })
-};
+export default getDirectMessageUsers;
--- src/views/DirectMessage.js
+++ src/views/DirectMessage.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import getDirectMessages from '../helpers/data/directMessageData';
+import getDirectMessageUsers from '../helpers/data/userData';
 
 function DirectMessage() {
   const [directMessages, setDirectMessages] = useState({});
+  const [directMessageUsers, setDirectMessageUsers] = useState({});
 
   useEffect(() => {
     getDirectMessages().then((response) => {
       console.warn(response);
       setDirectMessages(response);
       console.warn('dm', directMessages);
+    });
+
+    getDirectMessageUsers().then((response) => {
+      console.warn(response);
+      setDirectMessageUsers(response);
+      console.warn('dmu', directMessageUsers);
     });
   }, []);
 
